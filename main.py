cc5921e07647a1b6179c5d5b0e95e1deb7844821
JDKoder <JDKoder@diamond.com>
2025-06-05 13:41:54 -0400

    [CH2][L3]: Limiting fps to 60 and setting dt.

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
@@ -19,6 +23,7 @@ def main():
                 return
         player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 #60 fps
 
 
 if __name__ == "__main__":
