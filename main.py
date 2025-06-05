6f76729d4f549c56e33ff8b151bfd8538fe65cc7
JDKoder <JDKoder@diamond.com>
2025-06-05 13:29:32 -0400

    [CH2][L5]: Adding player to screen

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
@@ -11,11 +11,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.draw(screen)
         pygame.display.flip()
 
 
